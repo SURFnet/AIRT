43a4faa989057c1ecd3a7f364f00063b421bc216
Jeroen Hoppenbrouwers <hoppie@hoppie.nl>
2005-06-14 06:26:57 +0000

    Overview updated

--- doc/database/00-CHANGES.sql
+++ doc/database/00-CHANGES.sql
@@ -2,5 +2,5 @@
 ! This file contains all changed applied to the database schema since the
 ! last official release. The airtschema.sql file should always contain the
 ! database schema that results from applying all changes below.
-! When we release, this dbchanges.sql file will be copied to a new file
-! "from-prevRel-to-newRel.sql" and emptied.
+! When we release, this 00-CHANGES.sql file will be copied to a new file
+! "airtschema-from-prevRel-to-newRel.sql" and emptied.
