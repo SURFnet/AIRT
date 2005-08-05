be9e997236e63e53061c5d9e9cef2431a35a9bd1
Kees Leune <kees@leune.org>
2005-08-05 07:22:33 +0000

    netmasks accept CIDR notation

--- php/networks.php
+++ php/networks.php
@@ -1,7 +1,8 @@
 <?php
 /* vim: syntax=php tabstop=3 shiftwidth=3
  * AIRT: APPLICATION FOR INCIDENT RESPONSE TEAMS
- * Copyright (C) 2004,2005	Tilburg University, The Netherlands
+ * Copyright (C) 2004,2005   Tilburg University, The Netherlands
+ * TODO Codingstyle
 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -21,236 +22,231 @@
  * 
  * $Id$
  */
- require_once 'config.plib';
- require_once LIBDIR.'/airt.plib';
- require_once LIBDIR.'/database.plib';
- require_once LIBDIR.'/constituency.plib';
+require_once 'config.plib';
+require_once LIBDIR.'/airt.plib';
+require_once LIBDIR.'/database.plib';
+require_once LIBDIR.'/constituency.plib';
+require_once LIBDIR.'/network.plib';
 
- if (array_key_exists("action", $_REQUEST)) $action=$_REQUEST["action"];
- else $action = "list";
+if (array_key_exists("action", $_REQUEST)) {
+   $action=$_REQUEST["action"];
+} else {
+   $action = "list";
+}
 
- function show_form($id="") {
-    $label = "";
-    $action = "add";
-    $submit = "Add!";
-	$constituency = "";
-	$netmask = "";
-	$network = "";
+function show_form($id="") {
+   $label = "";
+   $action = "add";
+   $submit = "Add!";
+   $constituency = "";
+   $netmask = "";
+   $network = "";
 
-    if ($id != "") {
-        $networks = getNetworks();
-        if (array_key_exists($id, $networks))
-        {
-            $row = $networks["$id"];
-            $action = "update";
-            $submit = "Update!";
-            $network = $row["network"];
-            $netmask = $row["netmask"];
-            $label   = $row["label"];
-            $constituency = $row["constituency"];
-        }
-    }
-    echo <<<EOF
-<form action="$_SERVER[PHP_SELF]" method="POST">
-<input type="hidden" name="action" value="$action">
-<input type="hidden" name="id" value="$id">
-<table>
-<tr>
-    <td>Network Address</td>
-    <td><input type="text" size="30" name="network" value="$network"></td>
-</tr>
-<tr>
-    <td>Netmask</td>
-    <td><input type="text" size="30" name="netmask" value="$netmask"></td>
-</tr>
-<tr>
-    <td>Label</td>
-    <td><input type="text" size="30" name="label" value="$label"></td>
-</tr>
-<tr>
-    <td>Constituency</td>
-    <td>
-EOF;
-        print getConstituencySelection("constituency", $constituency);
-    echo <<<EOF
-</table>
-<p>
-<input type="submit" value="$submit">
-</form>
-EOF;
- }
+   if ($id != "") {
+      $networks = getNetworks();
+      if (array_key_exists($id, $networks)) {
+         $row = $networks["$id"];
+         $action = "update";
+         $submit = "Update!";
+         $network = $row["network"];
+         $netmask = $row["netmask"];
+         $label   = $row["label"];
+         $constituency = $row["constituency"];
+      }
+   }
+   print t('<form method="POST">')."\n";
+   print t('<input type="hidden" name="action" value="%action">', array(
+      '%action'=>$action))."\n";
+   print t('<input type="hidden" name="id" value="%id">', array(
+      '%id'=>$id))."\n";
+   print t('<table>')."\n";
+   print t('<tr>')."\n";
+   print t('   <td>Network Address</td>')."\n";
+   print t('   <td><input type="text" size="30" name="network" value="%network"></td>', array('%network'=>$network))."\n";
+   print t('</tr>')."\n";
+   print t('<tr>')."\n";
+   print t('   <td>Netmask or CIDR</td>')."\n";
+   print t('   <td><input type="text" size="30" name="netmask" value="%netmask"></td>', array('%netmask'=>$netmask))."\n";
+   print t('</tr>')."\n";
+   print t('<tr>')."\n";
+   print t('   <td>Label</td>')."\n";
+   print t('   <td><input type="text" size="30" name="label" value="%label"></td>', array('%label'=>$label))."\n";
+   print t('</tr>')."\n";
+   print t('<tr>')."\n";
+   print t('   <td>Constituency</td>'."\n");
+   print t('   <td>%constituencies</td>'."\n", array('%constituencies'=>getConstituencySelection("constituency", $constituency)))."\n";
+   print t('</tr>')."\n";
+   print t('</table>')."\n";
+   print t('<p/>')."\n";
+   print t('<input type="submit" value="%submit">', array('%submit'=>$submit))."\n";
+   print t('</form>')."\n";
+}
 
- /* very ugly function that is a first attempt to sort networks for the
-  * overview. Currently this is done by comparing the individual network bytes
-  * of the network address; the largest one comes first. 
-  * TODO: improve this code to make it IPv4-independent, and maybe take
-  * netmasks in account too. Also; there has to be a more elegant way of
-  * coding this.
-  */
- function airt_netsort($a, $b) {
-	 $ea = explode('.', $a['network']);
-	 $eb = explode('.', $b['network']);
 
-	 if ($ea[0] > $eb[0]) return 1;
-	 else if ($ea[0] < $eb[0]) return -1;
-	 else {
-		 if ($ea[1] > $eb[1]) return 1;
-		 else if ($ea[1] < $eb[1]) return -1;
-		 else {
-			 if ($ea[2] > $eb[2]) return 1;
-			 else if ($ea[2] < $eb[2]) return -1;
-			 else {
-				 if ($ea[3] > $eb[3]) return 1;
-				 else if ($ea[3] < $eb[3]) return -1;
-				 else return 0;
-			 }
-		 }
-	 }
- }
+switch ($action) {
+   // --------------------------------------------------------------
+   case "list":
+      pageHeader("Networks");
 
- switch ($action)
- {
-    // --------------------------------------------------------------
-    case "list":
-        pageHeader("Networks");
+      print t('<table cellpadding="3">')."\n";
+      print t('<tr>')."\n";
+      print t('   <td><B>Network</B></td>')."\n";
+      print t('   <td><B>Label</B></td>')."\n";
+      print t('   <td><B>Constituency</B></td>')."\n";
+      print t('   <td><B>Edit</B></td>')."\n";
+      print t('   <td><B>Delete</B></td>')."\n";
+      print t('</tr>')."\n";
 
-        echo <<<EOF
-<table cellpadding="3">
-<tr>
-    <td><B>Network</B></td>
-    <td><B>Netmask</B></td>
-    <td><B>Label</B></td>
-    <td><B>Constituency</B></td>
-    <td><B>Edit</B></td>
-    <td><B>Delete</B></td>
+      $networklist = getNetworks();
+      usort($networklist, "airt_netsort");
+      $constituencies = getConstituencies();
 
-</tr>
-EOF;
+      $count=0;
+      foreach ($networklist as $nid=>$data) {
+         $id = $data["id"];
+         $network      = $data["network"];
+         $netmask      = netmask2cidr($data["netmask"]);
+         $label        = $data["label"];
+         $constituency = $data["constituency"];
+         $constituency_name  = $constituencies["$constituency"]["name"];
+         $color = ($count++%2==0?"#FFFFFF":"#DDDDDD");
+         print t('<tr valign="top" bgcolor="%color">', array(
+            '%color'=>$color))."\n";
+         print t('<td>%network/%netmask</td>', array(
+            '%network'=>$network,
+            '%netmask'=>$netmask))."\n";
+         print t('<td>%label</td>', array('%label'=>$label))."\n";
+         print t('<td><a href="constituencies.php?action=edit&cons=%constituency">%name</a></td>', array(
+            '%constituency'=>$constituency,
+            '%name'=>$constituency_name))."\n";
+         print t('<td><a href="%url?action=edit&id=%id"><small>edit</small></td>', array('%url'=>$_SERVER['PHP_SELF'], '%id'=>$id))."\n";
+         print t('<td><a href="%url?action=delete&id=%id"><small>delete</small></td>', array('%url'=>$_SERVER['PHP_SELF'], '%id'=>$id))."\n";
+         print t('</tr>')."\n";
+      }
+      print t('</table>')."\n";
+      print t('<h3>New network</h3>')."\n";
+      show_form("");
+      break;
 
-        $networklist = getNetworks();
-		usort($networklist, "airt_netsort");
-        $constituencies = getConstituencies();
+   //-----------------------------------------------------------------
+   case "edit":
+      if (array_key_exists("id", $_GET)) {
+         $id=$_GET["id"];
+      } else {
+         airt_error(PARAM_MISSING, 'networks.php'.__LINE__);
+         Header("Location: $_SERVER[PHP_SELF]");
+         break;
+      }
+      pageHeader("Edit Network");
+      show_form($id);
+      pageFooter();
+      break;
 
-        $count=0;
-        foreach ($networklist as $nid=>$data) {
-			$id = $data["id"];
-            $network      = $data["network"];
-            $netmask      = $data["netmask"];
-            $label        = $data["label"];
-            $constituency = $data["constituency"];
-            $constituency_name  = $constituencies["$constituency"]["name"];
-            $color = ($count++%2==0?"#FFFFFF":"#DDDDDD");
-            echo <<<EOF
-<tr valign="top" bgcolor="$color">
-    <td>$network</td>
-    <td>$netmask</td>
-    <td>$label</td>
-    <td><a href="constituencies.php?action=edit&cons=$constituency"
-        >$constituency_name</a></td>
-    <td><a href="$_SERVER[PHP_SELF]?action=edit&id=$id"><small>edit</small></td>
-    <td><a href="$_SERVER[PHP_SELF]?action=delete&id=$id"><small>delete</small></td>
-</tr>
-EOF;
-        } // while $row
-        echo "</table>";
-
-        echo "<h3>New network</h3>";
-        show_form("");
-
-        break;
-
-    //-----------------------------------------------------------------
-    case "edit":
-        if (array_key_exists("id", $_GET)) $id=$_GET["id"];
-        else die("Missing information.");
-
-        pageHeader("Edit Network");
-        show_form($id);
-        pageFooter();
-        break;
-
-    //-----------------------------------------------------------------
-    case "add":
-    case "update":
-        if (array_key_exists("id", $_POST)) $id=$_POST["id"];
-        else $id="";
-        if (array_key_exists("network", $_POST)) $network=$_POST["network"];
-        else die("Missing information (1).");
-        if (array_key_exists("netmask", $_POST)) $netmask=$_POST["netmask"];
-        else die("Missing information (2).");
-        if (array_key_exists("label", $_POST)) $label=$_POST["label"];
-        else die("Missing information (3).");
-        if (array_key_exists("constituency", $_POST)) 
-            $constituency=$_POST["constituency"];
-        else die("Missing information (4).");
-
-        if ($action=="add")
-        {
-            # $conn = db_connect(DBDB, DBUSER, DBPASSWD)
-            # or die("Unable to connect to database.");
-
-            $res = db_query(sprintf("
-                INSERT INTO networks
-                (id, network, netmask, label, constituency)
-                VALUES
-                (nextval('networks_sequence'), %s, %s, %s, %s)",
-                    db_masq_null($network),
-                    db_masq_null($netmask),
-                    db_masq_null($label),
-                    $constituency
-                )
-            ) or die("Unable to excute query.");
-
-            # db_close($conn);
+   //-----------------------------------------------------------------
+   case "add":
+   case "update":
+      $missing = false;
+      if (array_key_exists("id", $_POST)) {
+         $id=$_POST["id"];
+      } else {
+         $id="";
+      }
+      if (array_key_exists("network", $_POST)) {
+         $network=$_POST["network"];
+      } else {
+         $missing = true;
+      }
+      if (array_key_exists("netmask", $_POST)) {
+         $netmask=$_POST["netmask"];
+         $res = array();
+         if (sscanf($netmask, "/%s", &$res) == 1) {
+            $netmask = cidr2netmask(substr($netmask, 1));
+         }
+      } else {
+         $missing = true;
+      }
+      if (array_key_exists("label", $_POST)) {
+         $label=$_POST["label"];
+      } else {
+         $missing = true;
+      }
+      if (array_key_exists("constituency", $_POST)) {
+         $constituency=$_POST["constituency"];
+      } else {
+         $missing = true;
+      }
+      if ($missing) {
+         airt_error(PARAM_MISSING, 'networks.php'.__LINE__);
+         Header("Location: $_SERVER[PHP_SELF]");
+         return;
+      }
+      if ($action=="add") {
+         $res = db_query(q('
+         INSERT INTO networks
+         (id, network, netmask, label, constituency)
+         VALUES
+         (nextval(\'networks_sequence\'), %network, %netmask, %label, %cons)',
+            array("%network"=>db_masq_null($network),
+               '%netmask'=>db_masq_null($netmask),
+               '%label'=>db_masq_null($label),
+               '%cons'=>$constituency)));
+         if (!$res) {
+            airt_error(DB_QUERY, 'networks.php'.__LINE__);
             Header("Location: $_SERVER[PHP_SELF]");
-        }
+            return;
+         }
 
-        else if ($action=="update")
-        {
-            if ($id=="") die("Missing information (5).");
-            # $conn = db_connect(DBDB, DBUSER, DBPASSWD)
-            # or die("Unable to connect to database.");
-
-            $res = db_query(sprintf("
-                UPDATE networks
-                SET    network=%s,
-                       netmask=%s,
-                       label=%s,
-                       constituency=%s
-                WHERE id=%s",
-                    db_masq_null($network),
-                    db_masq_null($netmask),
-                    db_masq_null($label),
-                    $constituency,
-                    $id
-                )
-            ) or die("Unable to excute query.");
-
+         Header("Location: $_SERVER[PHP_SELF]");
+      } elseif ($action=="update") {
+         if ($id=="") {
+            airt_error(PARAM_MISSING, 'networks.php'.__LINE__);
             Header("Location: $_SERVER[PHP_SELF]");
-        }
+            return;
+         }
+         $res = db_query(q('
+            UPDATE networks
+            SET    network=%network,
+                   netmask=%netmask,
+                   label=%label,
+                   constituency=%cons
+            WHERE id=%id', array(
+               '%network'=>db_masq_null($network),
+               '%netmask'=>db_masq_null($netmask),
+               '%label'=>db_masq_null($label),
+               '%cons'=>$constituency,
+               '%id'=>$id)));
+         if (!$res) {
+            airt_error(DB_QUERY, 'networks.php'.__LINE__);
+            Header("Location: $_SERVER[PHP_SELF]");
+            return;
+         }
+         Header("Location: $_SERVER[PHP_SELF]");
+      }
+      break;
 
-        break;
+   //-----------------------------------------------------------------
+   case "delete":
+      if (array_key_exists("id", $_GET)) {
+         $id=$_GET["id"];
+      } else {
+         airt_error(PARAM_MISSING, 'networks.php'.__LINE__);
+         Header("Location: $_SERVER[PHP_SELF]");
+         return;
+      }
+      $res = db_query(q('
+         DELETE FROM networks
+         WHERE  id=\'%id\'', array(
+            '%id'=>$id)));
+      if (!$res) {
+         airt_error(DB_QUERY, 'networks.php'.__LINE__);
+         Header("Location: $_SERVER[PHP_SELF]");
+         return;
+      }
+      Header("Location: $_SERVER[PHP_SELF]");
 
-    //-----------------------------------------------------------------
-    case "delete":
-        if (array_key_exists("id", $_GET)) $id=$_GET["id"];
-        else die("Missing information.");
-
-        # $conn = db_connect(DBDB, DBUSER, DBPASSWD)
-        # or die("Unable to connect to database.");
-
-        $res = db_query("
-            DELETE FROM networks
-            WHERE  id='$id'")
-        or die("Unable to execute query.");
-
-        # db_close($conn);
-        Header("Location: $_SERVER[PHP_SELF]");
-
-        break;
-    //-----------------------------------------------------------------
-    default:
-        die("Unknown action: $action");
- } // switch
-
+      break;
+   //-----------------------------------------------------------------
+   default:
+      die("Unknown action: $action");
+} // switch
 ?>
