adfb8c2b8d834d1564435d81d355111232f8eb11
Sebastiaan Tesink <sebas@uvt.nl>
2005-10-18 11:35:02 +0000

    added webservice-user to the database

--- doc/database/00-CHANGES.sql
+++ doc/database/00-CHANGES.sql
@@ -5,3 +5,20 @@
 -- When we release, this 00-CHANGES.sql file will be copied to a new file
 -- "airtschema-from-prevRel-to-newRel.sql" and emptied.
 ALTER TABLE incidents ADD COLUMN logging text;
+
+begin transaction;
+INSERT INTO users
+    (id, lastname, email, login, password)
+    VALUES
+    (nextval('users_sequence'), 'Webservice', 'airt-ws@example.com', 'webservice',
+    '62e91bc649621eed1004de8936987fc853df0eb7');
+INSERT INTO roles 
+    (id, label)
+    VALUES
+    (nextval('roles_sequence'), 'Webservice');
+INSERT INTO role_assignments
+    (id, role, userid)
+    VALUES
+    (nextval('role_assignments_sequence'), currval('roles_sequence'),
+    currval('users_sequence'));
+end transaction;
