f6e788e8890f74f858d93972171739de44da2148
Jeroen Hoppenbrouwers <hoppie@hoppie.nl>
2005-10-25 09:00:34 +0000

    VERSIONS added

--- doc/database/00-CHANGES.sql
+++ doc/database/00-CHANGES.sql
@@ -4,3 +4,19 @@
 -- database schema that results from applying all changes below.
 -- When we release, this 00-CHANGES.sql file will be copied to a new file
 -- "airtschema-from-prevRel-to-newRel.sql" and emptied.
+
+-- VERSIONS table tracks all relevant versions of the components.
+CREATE TABLE versions (
+  key   varchar(16) not null,
+  value varchar(16) not null,
+  primary key (key)
+)
+
+-- First release: insert the record. Needs one-time manual tweaking.
+INSERT INTO versions (key, value) 
+  VALUES ('airtschema','---schemastring---');
+
+-- When table and record has been established (next release):
+UPDATE versions SET value='---schemastring---' WHERE key='airtschema';
+-- Needs manual update with the AIRT_SCHEMA_VERSION string of the release.
+-- Cannot rely on .in expansion as it needs to stay fixed in history.
