ef21444f5ea0269d3cf44486b27c2b13d9f7ff7b
Jeroen Hoppenbrouwers <hoppie@hoppie.nl>
2005-10-25 11:58:56 +0000

    Back to AIRT_VERSION

--- doc/database/00-CHANGES.sql
+++ doc/database/00-CHANGES.sql
@@ -3,32 +3,34 @@
 -- last official release. The airtschema.sql file should always contain the
 -- database schema that results from applying all changes below.
 -- When we release, this 00-CHANGES.sql file will be copied to a new file
--- "airtschema-from-prevRel-to-newRel.sql" and emptied.
+-- "airtschema-from-prevRel-to-newRel.sql" and emptied. This should be done
+-- for EACH release, even if there are no changes to the database schema,
+-- because the VERSIONS table needs to be updated in all cases.
 
 -- VERSIONS table tracks all relevant versions of the components.
 CREATE TABLE versions (
-  key   varchar(16) not null,
-  value varchar(16) not null,
-  primary key (key)
+   key   varchar(16) not null,
+   value varchar(16) not null,
+   primary key (key)
 )
 
+-- MAILTEMPLATES table contains, eh, the, eh, mail templates.
 CREATE TABLE mailtemplates (
-   name varchar(80) not null,
-   body text not null,
-   createdby integer not null,
-   created timestamp not null,
+   name      varchar(80) not null,
+   body      text        not null,
+   createdby integer     not null,
+   created   timestamp   not null,
    updatedby integer,
-   updated timestamp,
+   updated   timestamp,
    primary key (name),
    foreign key (createdby) references users(id),
    foreign key (updatedby) references users(id)
 );
 
--- First release: insert the record. Needs one-time manual tweaking.
-INSERT INTO versions (key, value) 
-  VALUES ('airtschema','20051025.1');
+-- First release: insert the version record. Needs one-time manual tweaking.
+INSERT INTO versions (key, value) VALUES ('airtversion','20051025.1');
 
 -- When table and record has been established (next release):
-UPDATE versions SET value='---schemastring---' WHERE key='airtschema';
--- Needs manual update with the AIRT_SCHEMA_VERSION string of the release.
+UPDATE versions SET value='---versionstring---' WHERE key='airtversion';
+-- Needs manual update with the AIRT_VERSION string of the release.
 -- Cannot rely on .in expansion as it needs to stay fixed in history.
