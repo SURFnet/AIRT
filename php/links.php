997330208c78f7019306caa87aaf0490d694b4cd
Kees Leune <kees@leune.org>
2006-08-07 09:23:31 +0000

    more gettext'ed

--- php/links.php
+++ php/links.php
@@ -34,7 +34,7 @@ if (array_key_exists("action", $_REQUEST)) {
 }
 
 function format_position_select($current_value, $max) {
-   $out = choice("Do not display", '', $current_value);
+   $out = choice(_('Do not display'), '', $current_value);
    for ($i=1; $i <= $max; $i++) {
       $out .= choice($i, $i, $current_value);
    }
@@ -58,14 +58,14 @@ switch ($action) {
          Header("Location: index.php");
          return;
       }
-      $out = t("<p><strong>Main menu links</strong></p>\n");
-      $out .= t('<form method="POST">');
-      $out .= t("<table width=\"100%\">");
-      $out .= "<tr>\n";
-      $out .= "  <td><strong>Menu item</strong></td>\n";
-      $out .= "  <td><strong>Position</strong></td>\n";
-      $out .= "  <td colspan=\"2\">&nbsp;</td>\n";
-      $out .= "</tr>\n";
+      $out = '<p><strong>'._('Main menu links').'</strong></p>'.LF;
+      $out .= '<form method="POST">'.LF;
+      $out .= '<table width="100%">'.LF;
+      $out .= '<tr>'.LF;
+      $out .= '  <td><strong>'._('Menu item').'</strong></td>'.LF;
+      $out .= '  <td><strong>'._('Position').'</strong></td>'.LF;
+      $out .= '  <td colspan="2">&nbsp;</td>'.LF;
+      $out .= '</tr>'.LF;
 
       $count=0;
       while ($row = db_fetch_next($res)) {
@@ -81,16 +81,16 @@ switch ($action) {
          $out .= format_position_select($row['menu_position'], $n);
          $out .= "  </select>\n";
          $out .= "</td>\n";
-         $out .= t('<td><a href="%url?action=edit&id=%id">edit</a></td>',
+         $out .= t('<td><a href="%url?action=edit&id=%id">'._('edit').'</a></td>',
             array('%url'=>$_SERVER['PHP_SELF'],
                   '%id'=>urlencode($row["id"])));
-         $out .= t('<td><a href="%url?action=delete&id=%id">delete</a></td>',
+         $out .= t('<td><a href="%url?action=delete&id=%id">'._('delete').'</a></td>',
             array('%url'=>$_SERVER['PHP_SELF'],
                   '%id'=>urlencode($row["id"])));
          $out .= "</tr>\n";
       }
       $out .= "</table>\n";
-      $out .= "<p><input type=\"submit\" name=\"action\" value=\"Update main menu\"></p>";
+      $out .= "<p><input type=\"submit\" name=\"action\" value=\""._('Update main menu')."\"></p>";
       $out .= "</form>\n";
       $out .= "<hr/><br/>\n";
 
@@ -105,12 +105,12 @@ switch ($action) {
          Header("Location: index.php");
          return;
       }
-      $out .= t("<p><strong>Navbar links</strong></p>\n");
+      $out .= t("<p><strong>"._('Navbar links')."</strong></p>\n");
       $out .= t('<form method="POST">');
       $out .= t("<table width=\"100%\">");
       $out .= "<tr>\n";
-      $out .= "  <td><strong>Menu item</strong></td>\n";
-      $out .= "  <td><strong>Position</strong></td>\n";
+      $out .= "  <td><strong>"._('Menu item')."</strong></td>\n";
+      $out .= "  <td><strong>"._('Position')."</strong></td>\n";
       $out .= "  <td colspan=\"2\">&nbsp;</td>\n";
       $out .= "</tr>\n";
 
@@ -128,37 +128,38 @@ switch ($action) {
          $out .= format_position_select($row['navbar_position'], $n);
          $out .= "  </select>\n";
          $out .= "</td>\n";
-         $out .= t('<td><a href="%url?action=edit&id=%id">edit</a></td>',
+         $out .= t('<td><a href="%url?action=edit&id=%id">'._('edit').'</a></td>',
             array('%url'=>$_SERVER['PHP_SELF'],
                   '%id'=>urlencode($row["id"])));
-         $out .= t('<td><a href="%url?action=delete&id=%id">delete</a></td>',
+         $out .= t('<td><a href="%url?action=delete&id=%id">'._('delete').'</a></td>',
             array('%url'=>$_SERVER['PHP_SELF'],
                   '%id'=>urlencode($row["id"])));
          $out .= "</tr>\n";
       }
       $out .= "</table>\n";
-      $out .= "<p><input type=\"submit\" name=\"action\" value=\"Update navigation bar\"></p>";
+      $out .= "<p><input type=\"submit\" name=\"action\" value=\"".
+         _('Update navigation bar')."\"></p>";
       $out .= "</form>\n";
       $out .= "<hr/><br/>\n";
 
 
-      $out .= "<strong>Add new menu item</strong><BR/>\n";
+      $out .= "<strong>"._('Add new menu item')."</strong><BR/>\n";
       $out .= "<form method=\"POST\">\n";
       $out .= "<input type=\"hidden\" name=\"action\" value=\"add\">\n";
       $out .= "<table>\n";
       $out .= "<tr>\n";
-      $out .= "   <td>URL</td>\n";
+      $out .= "   <td>"._('URL')."</td>\n";
       $out .= "   <td><input type=\"text\" name=\"url\" size=\"50\"></td>\n";
       $out .= "</tr>\n";
       $out .= "<tr>\n";
-      $out .= "   <td>Description</td>\n";
+      $out .= "   <td>"._('Description')."</td>\n";
       $out .= "   <td><input type=\"text\" name=\"description\" size=\"50\"></td>\n";
       $out .= "</tr>\n";
       $out .= "</table>\n";
-      $out .= "<input type=\"submit\" value=\"Add\">\n";
+      $out .= "<input type=\"submit\" value=\""._('Add')."\">\n";
       $out .= "</form>\n";
 
-      pageHeader("Links");
+      pageHeader(_("Links"));
       print $out;
       pageFooter();
       break;
@@ -166,14 +167,11 @@ switch ($action) {
    // --------------------------------------------------------------
    case "add":
         if (array_key_exists("url", $_REQUEST)) $url = $_REQUEST["url"]
-        or die("Missing information (1).");
+        or die(_("Missing information (1)."));
 
         if (array_key_exists("description", $_REQUEST))
             $description = $_REQUEST["description"]
-        or die("Missing information (2).");
-
-        # $conn = db_connect(DBDB, DBUSER, DBPASSWD)
-        # or die("Unable to connect to database.");
+        or die(_("Missing information (2)."));
 
         $now = Date("Y-m-d H:i:s");
         $res = db_query(sprintf("
@@ -185,27 +183,21 @@ switch ($action) {
             db_masq_null($description),
             $now,
             $_SESSION["userid"]))
-        or die("Unable to insert URL");
+        or die(_("Unable to insert URL"));
 
-        # db_close($conn);
         Header("Location: $_SERVER[PHP_SELF]");
         break;
 
     // --------------------------------------------------------------
     case "delete":
         if (array_key_exists("id", $_REQUEST)) $id = $_REQUEST["id"]
-        or die("Missing information (1).");
-
-
-        # $conn = db_connect(DBDB, DBUSER, DBPASSWD)
-        # or die("Unable to connect to database.");
+        or die(_("Missing information (1)."));
 
         $res = db_query(sprintf("
             DELETE FROM urls
             WHERE ID=%s", $id))
-        or die("Unable to delete URL");
+        or die(_("Unable to delete URL"));
 
-        # db_close($conn);
         Header("Location: $_SERVER[PHP_SELF]");
         break;
 
@@ -213,61 +205,52 @@ switch ($action) {
     case "edit":
         if (array_key_exists("id", $_REQUEST))
             $id = $_REQUEST["id"]
-        or die("Missing information (3).");
-
-        # $conn = db_connect(DBDB, DBUSER, DBPASSWD)
-        # or die("Unable to connect to database.");
+        or die(_("Missing information (3)."));
 
         $res = db_query(sprintf("
             SELECT url, label
             FROM   urls
             WHERE  id=%s", $id))
-        or die("Unable to retrieve URL");
+        or die(_("Unable to retrieve URL"));
 
-        if (db_num_rows($res) == 0) die("Incorrect row id");
+        if (db_num_rows($res) == 0) die(_("Incorrect row id"));
 
         pageHeader("Edit link");
         $row = db_fetch_next($res);
-        # db_close($conn);
 
         $url = $row["url"];
         $description = $row["label"];
 
-        echo <<<EOF
-<form action="$_SERVER[PHP_SELF]" method="POST">
-<input type="hidden" name="action" value="update">
-<input type="hidden" name="id" value="$id">
-<table>
-<tr>
-    <td>URL</td>
-    <td><input type="text" name="url" size="50" value="$url"></td>
-</tr>
-<tr>
-    <td>Description</td>
-    <td><input type="text" name="description" size="50"
-         value="$description"></td>
-</tr>
-</table>
-<input type="submit" value="Update">
-</form>
-EOF;
+         print '<form action="'.$_SERVER[PHP_SELF].'" method="POST">'.LF;
+         print '<input type="hidden" name="action" value="update">'.LF;
+         print '<input type="hidden" name="id" value="'.$id.'">'.LF;
+         print '<table>'.LF;
+         print '<tr>'.LF;
+         print '    <td>URL</td>'.LF;
+         print '    <td><input type="text" name="url" size="50" value="'.$url.'"></td>'.LF;
+         print '</tr>'.LF;
+         print '<tr>'.LF;
+         print '    <td>Description</td>'.LF;
+         print '    <td><input type="text" name="description" size="50"'.LF;
+         print '         value="'.$description.'"></td>'.LF;
+         print '</tr>'.LF;
+         print '</table>'.LF;
+         print '<input type="submit" value="Update">'.LF;
+         print '</form>'.LF;
         break;
 
     // --------------------------------------------------------------
     case "update":
         if (array_key_exists("url", $_REQUEST)) $url = $_REQUEST["url"]
-        or die("Missing information (1).");
+        or die(_("Missing information (1)."));
 
         if (array_key_exists("description", $_REQUEST))
             $description = $_REQUEST["description"]
-        or die("Missing information (2).");
+        or die(_("Missing information (2)."));
 
         if (array_key_exists("id", $_REQUEST))
             $id = $_REQUEST["id"]
-        or die("Missing information (3).");
-
-        # $conn = db_connect(DBDB, DBUSER, DBPASSWD)
-        # or die("Unable to connect to database.");
+        or die(_("Missing information (3)."));
 
         $res = db_query(sprintf("
             UPDATE URLs
@@ -277,14 +260,13 @@ EOF;
             db_masq_null($description),
             db_masq_null($url),
             $id))
-        or die("Unable to update URL");
-        # db_close($conn);
+        or die(_("Unable to update URL"));
 
         Header("Location: $_SERVER[PHP_SELF]");
         break;
 
     // --------------------------------------------------------------
-    case "Update main menu":
+    case _("Update main menu"):
       if (!array_key_exists('menu_pos', $_POST)) {
          airt_error('PARAM_MISSING', 'links.php:'.__LINE__);
          Header("Location: $_SERVER[PHP_SELF]");
@@ -299,12 +281,12 @@ EOF;
             break;
          }
       }
-      airt_msg("Menu updated.");
+      airt_msg(_("Menu updated."));
       Header("Location: $_SERVER[PHP_SELF]");
       break;
 
     // --------------------------------------------------------------
-    case "Update navigation bar":
+    case _("Update navigation bar"):
       if (!array_key_exists('menu_pos', $_POST)) {
          airt_error('PARAM_MISSING', 'links.php:'.__LINE__);
          Header("Location: $_SERVER[PHP_SELF]");
@@ -319,11 +301,11 @@ EOF;
             break;
          }
       }
-      airt_msg("Navigation bar updated.");
+      airt_msg(_("Navigation bar updated."));
       Header("Location: $_SERVER[PHP_SELF]");
       break;
    // --------------------------------------------------------------
     default:
-        die("Unknown action: $action");
+        die(_("Unknown action: ").$action);
 } // switch
 ?>
