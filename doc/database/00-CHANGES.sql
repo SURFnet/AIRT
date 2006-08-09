07bffcc1a28da948a3c7113c50eb647f8f5e4090
Kees Leune <kees@leune.org>
2006-08-09 09:22:25 +0000

    language selection

--- doc/database/00-CHANGES.sql
+++ doc/database/00-CHANGES.sql
@@ -7,6 +7,8 @@
 -- for EACH release, even if there are no changes to the database schema,
 -- because the VERSIONS table needs to be updated in all cases.
 
+alter table users add column language varchar(10);
+
 UPDATE versions SET value='----version string----' WHERE key='airtversion';
 -- Needs manual update with the AIRT_VERSION string of the release.
 -- Cannot rely on .in expansion as it needs to stay fixed in history.
