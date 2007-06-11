30a9821da05ea55d27f9f018f2bc2753258a2904
Kees Leune <kees@leune.org>
2007-06-11 09:33:43 +0000

    new release. version 20070611.1

--- VERSION.m4
+++ VERSION.m4
@@ -1,1 +1,1 @@
-m4_define([AIRT_VERSION],[20070601.1])
+m4_define([AIRT_VERSION],[20070611.1])
--- doc/database/20070611.1.sql
+++ doc/database/20070611.1.sql
@@ -1,4 +1,4 @@
--- $Id$
+-- $Id: 00-CHANGES.sql 1097 2007-06-07 13:30:48Z kees $
 -- This file contains all changes applied to the database schema since the
 -- last official release. The airtschema.sql file should always contain the
 -- database schema that results from applying all changes below.
@@ -6,7 +6,19 @@
 -- "airtschema-newRel.sql" and emptied. This should be done
 -- for EACH release, even if there are no changes to the database schema,
 -- because the VERSIONS table needs to be updated in all cases.
+ALTER TABLE import_queue ADD COLUMN cache_mailtemplate VARCHAR(80);
+ALTER TABLE import_queue ADD COLUMN filter VARCHAR(80);
+ALTER TABLE import_queue ADD COLUMN version VARCHAR(80);
+CREATE SEQUENCE importqueue_templates_sequence;
+CREATE TABLE importqueue_templates (
+   id           integer,
+   filter       varchar(80) not null,
+   version      varchar(80),
+   mailtemplate varchar(80) not null,
+   primary key  (id),
+   foreign key  (mailtemplate) references mailtemplates(name)
+);
 
-UPDATE versions SET value='----version string----' WHERE key='airtversion';
+UPDATE versions SET value='20070611.1' WHERE key='airtversion';
 -- Needs manual update with the AIRT_VERSION string of the release.
 -- Cannot rely on .in expansion as it needs to stay fixed in history.
