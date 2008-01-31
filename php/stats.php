8f74149df096f8581a8278448a693ebf1408b49c
Kees Leune <kees@leune.org>
2008-01-31 21:03:22 +0000

    Created new report Created/open amount of incidents per constituency http://bugs.leune.com/cgi-bin/bugzilla/show_bug.cgi?id=2

--- php/stats.php
+++ php/stats.php
@@ -35,32 +35,32 @@ else $action='none';
  * @return A Unix timestamp containing the start date, or -1 in case of failure
  */
 function getStartDate() {
-  $year = fetchFrom('REQUEST', 'start_year', '%d');
-  $month = fetchFrom('REQUEST', 'start_month', '%d');
-  $day = fetchFrom('REQUEST', 'start_day', '%d');
+   $year = fetchFrom('REQUEST', 'start_year', '%d');
+   $month = fetchFrom('REQUEST', 'start_month', '%d');
+   $day = fetchFrom('REQUEST', 'start_day', '%d');
 
-  $start = strtotime(sprintf('%02d/%02d/%04d', $month, $day, $year));
-  if ($start == FALSE) {
-    return -1;
-  } else {
-    return $start;
-  }
+   $start = strtotime(sprintf('%02d/%02d/%04d', $month, $day, $year));
+   if ($start == FALSE) {
+      return -1;
+   } else {
+      return $start;
+   }
 }
 
 /** Extract the end date from the input form.
  * @return A Unix timestamp containing the end date, or -1 in case of failure
  */
 function getEndDate() {
-  $year = fetchFrom('REQUEST', 'stop_year', '%d');
-  $month = fetchFrom('REQUEST', 'stop_month', '%d');
-  $day = fetchFrom('REQUEST', 'stop_day', '%d');
+   $year = fetchFrom('REQUEST', 'stop_year', '%d');
+   $month = fetchFrom('REQUEST', 'stop_month', '%d');
+   $day = fetchFrom('REQUEST', 'stop_day', '%d');
 
-  $end = strtotime(sprintf('%02d/%02d/%04d', $month, $day, $year));
-  if ($end == FALSE) {
-    return -1;
-  } else {
-    return $end;
-  }
+   $end = strtotime(sprintf('%02d/%02d/%04d', $month, $day, $year));
+   if ($end == FALSE) {
+      return -1;
+   } else {
+      return $end;
+   }
 }
 
 
@@ -69,94 +69,95 @@ function getEndDate() {
  * of incidents in the reporting period.
  */
 function showMatrix($start, $end) {
-  $constituencies = getConstituencies();
-  $types = getIncidentTypes();
+   $constituencies = getConstituencies();
+   $types = getIncidentTypes();
 
-  $out = '<H2>'._('Incident type/Constituency matrix').'</H2>';
+   $out = '<H2>'._('Amount of incidents per incident type per Constituency matrix').'</H2>';
 
-  $out .= '<p>';
-  $out .= t(_('Printing from %startdate until %enddate.'), array(
-    '%startdate'=>Date('d-M-Y', $start),
-    '%enddate'=>Date('d-M-Y', $end)));
-  $out .= '</p>'.LF;
-  $out .= '<table border="1" cellpadding="3">'.LF;
-  $out .= '<tr>'.LF;
-  $out .= '<td>&nbsp;</td>'.LF;
-  foreach ($types as $t=>$label) {
-    $out .= t('<th>%t</th>'.LF, array('%t'=>$label));
-    $typesum[$label] = 0;
-  }
-  $out .= '<td><I>'._('Sum').'</I></td>'.LF;
-  $out .= '</tr>'.LF;
-  foreach ($constituencies as $consid=>$c) {
-    $constsum=0;
-    $out .= '<tr>'.LF;
-    $out .= t('<th>%c</th>'.LF, array('%c'=>$c['label']));
-    foreach ($types as $id=>$label) {
-      $res = db_query(q('SELECT COUNT(distinct i.id) AS count
-         FROM incidents i
-         LEFT JOIN incident_addresses a ON (i.id = a.incident)
-         WHERE type = %type
-         AND a.constituency = %constituency
-         AND i.created BETWEEN \'%start\' AND \'%stop\'', array(
-            '%type'=>$id, 
-            '%constituency'=>$consid,
-            '%start'=>Date('d-M-Y', $start),
-            '%stop'=>Date('d-M-Y', $end))));
-      $row = db_fetch_next($res);
-      $out .= '<td>'.$row['count'].'</td>';
-      $typesum[$label] += $row['count'];
-      $constsum += $row['count'];
-      db_free_result($res);
-    }
-    $out .= '<td><I>'.$constsum.'</I></td>'.LF;
-    $out .= '</tr>'.LF;
-  }
-  $out .= '<tr>'.LF;
-  $out .= '<td>'._('Sum').'</td>'.LF;
-  $sum = 0;
-  foreach ($types as $id=>$label) {
-    $out .= '<td><I>'.$typesum[$label].'</I></td>'.LF;
-    $sum += $typesum[$label];
-  }
-  $out .= '<td><I><B>'.$sum.'</B></I></td>'.LF;
-  $out .= '<tr><td/>'.LF;
-  foreach ($types as $t=>$label) {
-    $out .= t('<th>%t</th>'.LF, array('%t'=>$label));
-    $typesum[$label] = 0;
-  }
-  $out .= '</tr>'.LF;
-  $out .= '</table>'.LF;
+   $out .= '<p>';
+   $out .= _('Report generated on ').date('r').'.<br/>'.LF;
+   $out .= t(_('Printing incidents from %startdate until %enddate.<br/>'), array(
+      '%startdate'=>Date('d-M-Y', $start),
+      '%enddate'=>Date('d-M-Y', $end)));
+   $out .= '</p>'.LF;
+   $out .= '<table border="1" cellpadding="3">'.LF;
+   $out .= '<tr>'.LF;
+   $out .= '<td>&nbsp;</td>'.LF;
+   foreach ($types as $t=>$label) {
+      $out .= t('<th>%t</th>'.LF, array('%t'=>$label));
+      $typesum[$label] = 0;
+   }
+   $out .= '<td><I>'._('Sum').'</I></td>'.LF;
+   $out .= '</tr>'.LF;
+   foreach ($constituencies as $consid=>$c) {
+      $constsum=0;
+      $out .= '<tr>'.LF;
+      $out .= t('<th>%c</th>'.LF, array('%c'=>$c['label']));
+      foreach ($types as $id=>$label) {
+         $res = db_query(q('SELECT COUNT(distinct i.id) AS count
+            FROM incidents i
+            LEFT JOIN incident_addresses a ON (i.id = a.incident)
+            WHERE type = %type
+            AND a.constituency = %constituency
+            AND i.created BETWEEN \'%start\' AND \'%stop\'', array(
+               '%type'=>$id, 
+               '%constituency'=>$consid,
+               '%start'=>Date('d-M-Y', $start),
+               '%stop'=>Date('d-M-Y', $end))));
+         $row = db_fetch_next($res);
+         $out .= '<td>'.$row['count'].'</td>';
+         $typesum[$label] += $row['count'];
+         $constsum += $row['count'];
+         db_free_result($res);
+      }
+      $out .= '<td><I>'.$constsum.'</I></td>'.LF;
+      $out .= '</tr>'.LF;
+   }
+   $out .= '<tr>'.LF;
+   $out .= '<td>'._('Sum').'</td>'.LF;
+   $sum = 0;
+   foreach ($types as $id=>$label) {
+      $out .= '<td><I>'.$typesum[$label].'</I></td>'.LF;
+      $sum += $typesum[$label];
+   }
+   $out .= '<td><I><B>'.$sum.'</B></I></td>'.LF;
+   $out .= '<tr><td/>'.LF;
+   foreach ($types as $t=>$label) {
+      $out .= t('<th>%t</th>'.LF, array('%t'=>$label));
+      $typesum[$label] = 0;
+   }
+   $out .= '</tr>'.LF;
+   $out .= '</table>'.LF;
 
-  $out .= '<H2>'._('Incidents without IP addresses').'</h2>';
-  $res = db_query(q('SELECT i.id
-     FROM incidents i
-     LEFT JOIN incident_addresses a ON i.id = a.incident
-     WHERE i.created BETWEEN \'%start\' AND \'%stop\'
-     GROUP BY i.id
-     HAVING COUNT (ip) = 0', array(
-            '%start'=>Date('d-M-Y', $start),
-            '%stop'=>Date('d-M-Y', $end))));
-  $out .= '<table border="1" cellpadding="3">'.LF;
-  $out .= '<tr>'.LF;
-  $out .= '   <th>'._('Incident ID').'</th>'.LF;
-  $out .= '   <th>'._('Type').'</th>'.LF;
-  $out .= '   <th>'._('Status').'</th>'.LF;
-  $out .= '   <th>'._('State').'</th>'.LF;
-  $out .= '   <th>'._('Date').'</th>'.LF;
-  $out .= '</tr>'.LF;
-  while ($row = db_fetch_next($res)) {
-    $i = getIncident($row['id']);
-    $out .= '<tr>'.LF;
-    $out .= '<td>'.normalize_incidentid($row['id']).'</td>'.LF;
-    $out .= '<td>'.getIncidentTypeLabelByID($i['type']).'</td>'.LF;
-    $out .= '<td>'.getIncidentStatusLabelByID($i['status']).'</td>'.LF;
-    $out .= '<td>'.getIncidentStateLabelByID($i['status']).'</td>'.LF;
-    $out .= '<td>'.($i['incidentdate'] == 0 ? _('Unknown') : date('d-M-Y H:i:s', $i['incidentdate'])).'</td>'.LF;
-    $out .= '</tr>'.LF;
-  }
-  $out .= '</table>'.LF;
-  print $out;
+   $out .= '<H2>'._('Incidents without IP addresses').'</h2>';
+   $res = db_query(q('SELECT i.id
+      FROM incidents i
+      LEFT JOIN incident_addresses a ON i.id = a.incident
+      WHERE i.created BETWEEN \'%start\' AND \'%stop\'
+      GROUP BY i.id
+      HAVING COUNT (ip) = 0', array(
+         '%start'=>Date('d-M-Y', $start),
+         '%stop'=>Date('d-M-Y', $end))));
+   $out .= '<table border="1" cellpadding="3">'.LF;
+   $out .= '<tr>'.LF;
+   $out .= '   <th>'._('Incident ID').'</th>'.LF;
+   $out .= '   <th>'._('Type').'</th>'.LF;
+   $out .= '   <th>'._('Status').'</th>'.LF;
+   $out .= '   <th>'._('State').'</th>'.LF;
+   $out .= '   <th>'._('Date').'</th>'.LF;
+   $out .= '</tr>'.LF;
+   while ($row = db_fetch_next($res)) {
+      $i = getIncident($row['id']);
+      $out .= '<tr>'.LF;
+      $out .= '<td>'.normalize_incidentid($row['id']).'</td>'.LF;
+      $out .= '<td>'.getIncidentTypeLabelByID($i['type']).'</td>'.LF;
+      $out .= '<td>'.getIncidentStatusLabelByID($i['status']).'</td>'.LF;
+      $out .= '<td>'.getIncidentStateLabelByID($i['status']).'</td>'.LF;
+      $out .= '<td>'.($i['incidentdate'] == 0 ? _('Unknown') : date('d-M-Y H:i:s', $i['incidentdate'])).'</td>'.LF;
+      $out .= '</tr>'.LF;
+   }
+   $out .= '</table>'.LF;
+   print $out;
 }
 
 function printStatsInputForm() {
@@ -180,7 +181,7 @@ function printStatsInputForm() {
   $out = '<P>'._('Please select the reporting period of which you would like to see statistics. (note; the start date and the end date are included in the
 report.').'</P>';
 
-  $out .= '<form action="'.$_SERVER[PHP_SELF].'" method="POST">'.LF;
+  $out .= '<form action="'.$_SERVER['PHP_SELF'].'" method="POST">'.LF;
   $out .= '<table>'.LF;
   $out .= '<tr>'.LF;
   $out .= '   <td>'._('Start date (day-month-year)').'</td>'.LF;
@@ -225,37 +226,111 @@ report.').'</P>';
   $out .= '</tr>'.LF;
   $out .= '</table>'.LF;
 
+  $out .= _('Report to generate').LF;
+  $out .= '<select name="report">'.LF;
+  $out .= '<option value="0" SELECTED>--- Select report ---</option>'.LF;
+  $out .= '<option value="1">'.
+     _('Amount per incident type per constituency').'</option>'.LF;
+  $out .= '<option value="2">'.
+     _('Created/open amount per constituency').'</option>'.LF;
+  $out .= '</select>'.LF;
+
   $out .= '<p><input type="submit" name="action" value="'._('Show statistics').'">'.LF;
   $out .= '</form>'.LF;
   print $out;
 }
 
+function printreport1($start, $stop) {
+   $startdate = date('r', $start);
+   $enddate = date('r', $stop);
+   $out = '<p>'._("Statistics from $startdate to $enddate.").'</p>';
+   showmatrix($start, $stop);
+   pageFooter();
+   return;
+}
+
+function printreport2($start, $stop) {
+   $out = '<H2>'._('Created/open amount of incidents per constituency').'</H2>';
+   $out .= _('Report generated on ').date('r').'.<br/>'.LF;
+   $out .= t(_('Printing incidents from %startdate until %enddate.').
+      '<br/>'.LF, array(
+      '%startdate'=>Date('d-M-Y', $start),
+      '%enddate'=>Date('d-M-Y', $stop)));
+   $out .= '<p/>'.LF;
+   $const = getconstituencies();
+   $q = q('select c.id as constituencyid, count(i.id) as count
+      from incidents i
+      left join incident_addresses ia on ia.incident = i.id
+      left join constituencies c on ia.constituency = c.id
+      where created between \'%begin\' and \'%end\'
+      group by c.id', array(
+         '%begin'=>Date('d-M-Y', $start),
+         '%end'=>Date('d-M-Y', $stop)));
+   $res = db_query($q);
+   $out .= '<table border="1">'.LF;
+   $out .= '<tr>'.LF;
+   $out .= '   <th>&nbsp;</th>'.LF;
+   $out .= '   <th colspan="2">'._('Incidents').'</th>'.LF;
+   $out .= '</tr>'.LF;
+   $out .= '<tr>'.LF;
+   $out .= '   <th>'._('Constituency').'</th>'.LF;
+   $out .= '   <th>'._('created').'</th>'.LF;
+   $out .= '   <th>'._('open').'</th>'.LF;
+   $out .= '</tr>'.LF;
+
+   while ($row = db_fetch_next($res)) {
+      if (empty($row['constituencyid'])) continue;
+      $o = getOpenIncidentsByConstituency($row['constituencyid']);
+      $out .= '<tr>'.LF;
+      $out .= t('<td>%cons</td>', array(
+         '%cons'=>$const[$row['constituencyid']]['label'])).LF;
+      $out .= t('<td>%created</td>', array(
+         '%created'=>$row['count'])).LF;
+      $out .= t('<td>%open</td>', array(
+         '%open'=>sizeof($o))).LF;
+      $out .= '</tr>'.LF;
+   }
+   $out .= '</table>'.LF;
+
+   print $out;
+   pageFooter();
+   return;
+}
+
 /*****************************************************************************/
 switch ($action) {
-	case 'none':
+   case 'none':
     printStatsInputForm();
-		break;
-	case _('Show statistics'):
-		pageHeader('AIRT statistics');
-
-    $start = getStartDate();
-    if ($start == -1) {
-      print 'Invalid date format of start date.';
       break;
-    }
-    $stop = getEndDate();
-    if ($stop == -1) {
-      print 'Invalid date format of end date.';
-      break;
-    }
+   case _('Show statistics'):
+      pageHeader('AIRT statistics');
+      $start = getStartDate();
+      $stop = getEndDate();
+      if ($start == -1) {
+         print 'Invalid date format of start date.';
+         break;
+      }
+      if ($stop == -1) {
+         print 'Invalid date format of end date.';
+         break;
+      }
 
-    $startdate = date('r', $start);
-    $enddate = date('r', $stop);
-		$out = '<p>'._("Statistics from $startdate to $enddate.").'</p>';
-      showmatrix($start, $stop);
-		pageFooter();
-		break;
-	default:
-		die('Unknown action');
+      switch (fetchFrom('POST', 'report', '%d')) {
+         case 0:
+            reload();
+            break;
+         case 1:
+            printreport1($start, $stop);
+            break;
+         case 2:
+            printreport2($start, $stop);
+            break;
+         default:
+            reload();
+      }
+      break;
+
+   default:
+      die('Unknown action');
 }
 ?>
