b287377b35490f3281f34e41eefbd5443d04a426
Kees Leune <kees@leune.org>
2008-07-31 14:49:47 +0000

    fixes in airtschema.sql

--- VERSION.m4
+++ VERSION.m4
@@ -1,1 +1,1 @@
-m4_define([AIRT_VERSION],[20080730.1])
+m4_define([AIRT_VERSION],[20080731.1])
---
+++ doc/database/20080731.1.sql
@@ -0,0 +1,11 @@
+-- $Id: 00-CHANGES.sql 1262 2008-07-30 23:58:58Z kees $
+-- This file contains all changes applied to the database schema since the
+-- last official release. The airtschema.sql file should always contain the
+-- database schema that results from applying all changes below.
+-- When we release, this 00-CHANGES.sql file will be copied to a new file
+-- "airtschema-newRel.sql" and emptied. This should be done
+-- for EACH release, even if there are no changes to the database schema,
+-- because the VERSIONS table needs to be updated in all cases.
+UPDATE versions SET value='----version----' WHERE key='airtversion';
+-- Needs manual update with the AIRT_VERSION string of the release.
+-- Cannot rely on .in expansion as it needs to stay fixed in history.
