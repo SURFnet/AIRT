bcdc96f83f97fe75c8d807e7a10bb88178670bc9
Kees Leune <kees@leune.org>
2008-11-16 13:41:01 +0000

    backend code for associating emails with incidents

--- doc/database/00-CHANGES.sql
+++ doc/database/00-CHANGES.sql
@@ -6,24 +6,14 @@
 -- "airtschema-newRel.sql" and emptied. This should be done
 -- for EACH release, even if there are no changes to the database schema,
 -- because the VERSIONS table needs to be updated in all casesA
-CREATE TABLE mailbox (
-    id    integer,      -- pull from generic_sequence
-    messageid varchar,  -- Message-Id header
-    sender    varchar,  -- From header (not envelope from)
-    recipient varchar,  -- To header
-    date      numeric,
-    subject   varchar,  -- Subject header
-    body      varchar,  -- Everything not header (no mime parsing yet)
-    status    varchar,
-    PRIMARY KEY (id)
+CREATE TABLE incident_mail (
+    id integer,
+    messageid integer,
+    incidentid integer,
+    PRIMARY KEY (id),
+    FOREIGN KEY (messageid) REFERENCES mailbox(id),
+    FOREIGN KEY (incidentid) REFERENCES incidents(id)
 );
-
-ALTER TABLE import_queue 
-   ADD COLUMN metatype VARCHAR;
-UPDATE import_queue 
-   SET metatype='incident' 
-   WHERE metatype IS NULL;
-
 
 UPDATE versions SET value='----version----' WHERE key='airtversion';
 -- Needs manual update with the AIRT_VERSION string of the release.
--- doc/database/airtperms.sql
+++ doc/database/airtperms.sql
@@ -27,6 +27,8 @@ grant select,insert,update,delete on roles to airt;
 grant select,insert,update,delete on urls to airt;
 grant select,insert,update,delete on user_comments to airt;
 grant select,insert,update,delete on users to airt;
+grant select,insert,update,delete on mailbox to airt;
+grant select,insert,update,delete on incident_mail to airt;
 grant select on versions to airt;
 
 grant select,update on address_roles_sequence to airt;
