c94a5d431ef92fe583b9131c6a66b8e0476e3e55
Kees Leune <kees@leune.org>
2011-04-18 20:17:09 +0000

    maximum bubble size is now configurable as a global option. Constituency in import queue now also has a hover bubble if constituency notes are defined

--- doc/database/00-CHANGES.sql
+++ doc/database/00-CHANGES.sql
@@ -8,6 +8,7 @@
 -- because the VERSIONS table needs to be updated in all casesA
 
 UPDATE versions SET value='----version----' WHERE key='airtversion';
+INSERT into settings (key) values ('bubblesize');
 -- Needs manual update with the AIRT_VERSION string of the release.
 -- Cannot rely on .in expansion as it needs to stay fixed in history.
 
