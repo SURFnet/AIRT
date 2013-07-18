3fd3d5f85695280be2610e4e01caf0ccb2d8229a
Kees Leune <kees@leune.com>
2013-07-18 04:44:08 -0400

    skip emails when an error occurs during sending; and clean the array when done

--- php/mailtemplates.php
+++ php/mailtemplates.php
@@ -1,6 +1,6 @@
 <?php
 /* vim: syntax=php tabstop=3 shiftwidth=3
- * $Id$ 
+ * $Id: mailtemplates.php 1556 2011-12-18 14:48:07Z kees $ 
  * mailtemplates.php - Standard messages
  *
  * AIRT: APPLICATION FOR INCIDENT RESPONSE TEAMS
@@ -303,44 +303,49 @@ special variables in the template:').'<p>'.LF;
          reload();
          return;
       }
-      /* comma separated list of receipients. */
-      $to = strip_tags(fetchFrom('POST', 'to'));
-      if (empty($to)) {
-         airt_error('PARAM_MISSING', 'mailtemplates.php:'.__LINE__);
-         airt_error(_('No receiver specified (to: field is missing'));
-         reload();
-         return;
-      }
-      $subject = strip_tags(fetchFrom('POST', 'subject'));
-      if (empty($subject)) {
-         airt_error('PARAM_MISSING', 'mailtemplates.php:'.__LINE__);
-         reload();
-         return;
-      }
-      $msg = fetchFrom('POST', 'msg');
-      if (empty($msg)) {
-         airt_error('PARAM_MISSING', 'mailtemplates.php:'.__LINE__);
-         reload();
-         return;
-      }
-      $sign = strip_tags(fetchFrom('POST', 'sign'));
-      if (empty($sign)) {
-         $sign = 'off';
-      }
       $incidentid = fetchFrom('POST', 'incidentid', '%d');
       if (empty($incidentid)) {
          airt_error('PARAM_MISSING', 'mailtemplates.php:'.__LINE__);
          reload();
          return;
       }
+		$override = fetchFrom('REQUEST', 'override', '%d');
+		defaultTo($override, 0);
 
-      $incidentids = fetchFrom('POST', 'incidentids');
+		if (!array_key_exists('skipped', $_SESSION)) {
+			$_SESSION['skipped'] = array();
+		}
+		$skipped = false;
+
+      /* comma separated list of receipients. */
+      $to = strip_tags(fetchFrom('POST', 'to'));
+      if (empty($to)) {
+			$_SESSION['skipped'][$incidentid] = 1;
+			$skipped = true;
+      }
+      $subject = strip_tags(fetchFrom('POST', 'subject'));
+      if (empty($subject)) {
+			$_SESSION['skipped'][$incidentid] = 1;
+			$skipped = true;
+      }
+      $msg = fetchFrom('POST', 'msg');
+      if (empty($msg)) {
+			$_SESSION['skipped'][$incidentid] = 1;
+			$skipped = true;
+         return;
+      }
+      $sign = strip_tags(fetchFrom('POST', 'sign'));
+      if (empty($sign)) {
+         $sign = 'off';
+      }
+
+      $incidentids = fetchFrom('SESSION', 'incidentids');
       // do not strip_tags here; it will break the email address
       $replyto = fetchFrom('POST', 'replyto');
       defaultTo($replyTo, '');
-      $template = strip_tags(fetchFrom('POST', 'template'));
+      $template = strip_tags(fetchFrom('SESSION', 'template'));
 
-      $autosend = fetchFrom('POST', 'autosend');
+      $autosend = fetchFrom('SESSION', 'autosend');
       defaultTo($autosend, 'no');
       if (array_search($autosend, array('yes', 'no')) === FALSE) {
          $autosend = 'no';
@@ -348,7 +353,8 @@ special variables in the template:').'<p>'.LF;
 
       /* prevent sending bogus stuff */
       if (trim($to) == '') {
-         die(_('Empty recipient?'));
+			$_SESSION['skipped'][$incidentid] = 1;
+			$skipped = true;
       }
       $to = explode(',', $to);
       foreach ($to as $key=>$value) {
@@ -359,192 +365,216 @@ special variables in the template:').'<p>'.LF;
       $to = implode(',', $to);
 
       if (trim($msg) == '') {
-         die(_('Empty message body?'));
+			$_SESSION['skipped'][$incidentid] = 1;
+			$skipped = true;
       }
 
-      /* clean off html and stuff (only unformatted mail) */
-      $msg = stripslashes($msg);
-      $msg = str_replace("\r", '', $msg);
 
-      /* prepare the intial state of the message */
-      $hdrs = array(
-         'From'     => $from,
-         'Subject'  => $subject,
-         'To'       => $to,
-         'X-Mailer' => 'AIRT $Revision$ http://www.airt.nl',
-         'MIME-Version' => '1.0',
-      );
-      if ($replyto != '') {
-          $hdrs['Reply-To'] = $replyto;
-      }
+		if (! $skipped) {
+			/* clean off html and stuff (only unformatted mail) */
+			$msg = stripslashes($msg);
+			$msg = str_replace("\r", '', $msg);
 
-      /* set up mail recipient */
-      Setup::getOption('mailcc', $mailcc, true);
-      if (!empty($mailcc)) {
-         $mailto = array($to, $mailcc);
-         $hdrs["Cc"] = $mailcc;
-      } else {
-         $mailto = array($to);
-      }
+			/* prepare the intial state of the message */
+			$hdrs = array(
+				'From'     => $from,
+				'Subject'  => $subject,
+				'To'       => $to,
+				'X-Mailer' => 'AIRT $Revision: 1556 $ http://www.airt.nl',
+				'MIME-Version' => '1.0',
+			);
+			if ($replyto != '') {
+				 $hdrs['Reply-To'] = $replyto;
+			}
 
-      Setup::getOption('mailenvfrom', $mailenvfrom, true);
-      /* set up envelope sender */
-      $envfrom="-f".$mailenvfrom;
+			/* set up mail recipient */
+			Setup::getOption('mailcc', $mailcc, true);
+			if (!empty($mailcc)) {
+				$mailto = array($to, $mailcc);
+				$hdrs["Cc"] = $mailcc;
+			} else {
+				$mailto = array($to);
+			}
 
-      /* will send via Mail class */
-      $mail_params = array(
-         'sendmail_args' => $envfrom,
-         'sendmail_path' => SENDMAIL,
-      );
+			Setup::getOption('mailenvfrom', $mailenvfrom, true);
+			/* set up envelope sender */
+			$envfrom="-f".$mailenvfrom;
 
-      $msg_params = array();
-      $msg_params['content_type'] = 'multipart/mixed';
-      $msg_params['disposition'] = 'inline';
+			/* will send via Mail class */
+			$mail_params = array(
+				'sendmail_args' => $envfrom,
+				'sendmail_path' => SENDMAIL,
+			);
+
+			$msg_params = array();
+			$msg_params['content_type'] = 'multipart/mixed';
+			$msg_params['disposition'] = 'inline';
 
 
-      $attachcount=0;
+			$attachcount=0;
 
-      if ($sign == 'off') {
-         $msg_params['content_type'] = 'text/plain';
-         unset($msg_params['disposition']);
-         $mime = new Mail_mimePart($msg, $msg_params);
-         $m = $mime->encode();
-         $body = $m['body'];
-      } else {
-         // pgp signed messages are described in RFC 2015
-         $msg_params['content_type'] = 'multipart/signed; micalg=pgp-sha1; protocol="application/pgp-signature"';
-         $mime = new Mail_mimePart('This is an OpenPGP/MIME signed message (RFC2440 and 3156)', $msg_params);
+			if ($sign == 'off') {
+				$msg_params['content_type'] = 'text/plain';
+				unset($msg_params['disposition']);
+				$mime = new Mail_mimePart($msg, $msg_params);
+				$m = $mime->encode();
+				$body = $m['body'];
+			} else {
+				// pgp signed messages are described in RFC 2015
+				$msg_params['content_type'] = 'multipart/signed; micalg=pgp-sha1; protocol="application/pgp-signature"';
+				$mime = new Mail_mimePart('This is an OpenPGP/MIME signed message (RFC2440 and 3156)', $msg_params);
 
-         // MIME encoding requires CR/LF; see RFC2015
-         $msg = explode("\n",$msg);
-         $msg = implode("\r\n",$msg);
+				// MIME encoding requires CR/LF; see RFC2015
+				$msg = explode("\n",$msg);
+				$msg = implode("\r\n",$msg);
 
-         $body_params = array();
-         $body_params['content_type'] = 'text/plain';
-         $body_params['disposition'] = 'inline';
-         $body_params['charset'] = 'ISO-8859-1';
-         $body_params['encoding'] = 'quoted-printable';
-         $mime->addsubpart($msg, $body_params);
+				$body_params = array();
+				$body_params['content_type'] = 'text/plain';
+				$body_params['disposition'] = 'inline';
+				$body_params['charset'] = 'ISO-8859-1';
+				$body_params['encoding'] = 'quoted-printable';
+				$mime->addsubpart($msg, $body_params);
 
-         /* message signature */
-         $sig_params = array();
-         $sig_params['content_type'] = 'application/pgp-signature';
-         $sig_params['description'] = _('Digital Signature');
-         $mime->addsubpart('@AIRT-SIGNATURE@', $sig_params);
-         $m = $mime->encode();
+				/* message signature */
+				$sig_params = array();
+				$sig_params['content_type'] = 'application/pgp-signature';
+				$sig_params['description'] = _('Digital Signature');
+				$mime->addsubpart('@AIRT-SIGNATURE@', $sig_params);
+				$m = $mime->encode();
 
-         // now generate the signature and replace placeholder
-         // 1. Extract the delimiter string
-         $disp = explode(';', $m['headers']['Content-Type']);
-         if (preg_match('/="(.*)"$/', $disp[3], $match) > 0) {
-            $delimiter = $match[1];
-         } else {
-            $delimiter = 'XXX'; // this means MIME is broken!
-         }
+				// now generate the signature and replace placeholder
+				// 1. Extract the delimiter string
+				$disp = explode(';', $m['headers']['Content-Type']);
+				if (preg_match('/="(.*)"$/', $disp[3], $match) > 0) {
+					$delimiter = $match[1];
+				} else {
+					$delimiter = 'XXX'; // this means MIME is broken!
+				}
 
-         // 2. Extract the main body part
-         $body = $m['body'];
-         $msg = preg_split('/[-][-]/'.$delimiter, $body);
-         $msgbody = implode("\r\n", array_slice(explode("\r\n", $msg[1]), 1));
-         $msgbody = substr($msgbody, 0, -1);
+				// 2. Extract the main body part
+				$body = $m['body'];
+				$msg = preg_split('/[-][-]/'.$delimiter, $body);
+				$msgbody = implode("\r\n", array_slice(explode("\r\n", $msg[1]), 1));
+				$msgbody = substr($msgbody, 0, -1);
 
-         // 3. Sign the main body part and capture the signature
-         // create mime-body and remove delimiting lines. RFC 2015 requires
-         // that the message is signed, including its MIME headers
+				// 3. Sign the main body part and capture the signature
+				// create mime-body and remove delimiting lines. RFC 2015 requires
+				// that the message is signed, including its MIME headers
 
-         /* write msg to temp file */
-         $fname = tempnam('/tmp', 'airt_');
-         $f = fopen($fname, 'w');
-         fwrite($f, $msgbody, strlen($msgbody));
-         fclose($f);
+				/* write msg to temp file */
+				$fname = tempnam('/tmp', 'airt_');
+				$f = fopen($fname, 'w');
+				fwrite($f, $msgbody, strlen($msgbody));
+				fclose($f);
 
-         // 4. update the footer
+				// 4. update the footer
 
-         /* invoke gpg */
-         $cmd = sprintf("%s %s --homedir %s --default-key %s %s",
-            GPG_BIN, GPG_OPTIONS, GPG_HOMEDIR, GPG_KEYID, $fname);
-         exec($cmd);
-         if (($sig = file_get_contents("$fname.asc")) == false) {
-            die(_('Unable to read signed message.'));
-         }
+				/* invoke gpg */
+				$cmd = sprintf("%s %s --homedir %s --default-key %s %s",
+					GPG_BIN, GPG_OPTIONS, GPG_HOMEDIR, GPG_KEYID, $fname);
+				exec($cmd);
+				if (($sig = file_get_contents("$fname.asc")) == false) {
+					die(_('Unable to read signed message.'));
+				}
 
-         /* clean up */
-         unlink($fname);
-         unlink("$fname.asc");
-         $body = preg_replace("/@AIRT-SIGNATURE@/", $sig, $body);
-      }
+				/* clean up */
+				unlink($fname);
+				unlink("$fname.asc");
+				$body = preg_replace("/@AIRT-SIGNATURE@/", $sig, $body);
+			}
 
-      $mail = Mail::factory('sendmail', $mail_params);
-      if ($mail !== TRUE && PEAR::isError($mail)) {
-          airt_msg(_("Error creating mail relay!"). ' - '.$mail->getMessage());
-          exit(reload());
-      }
-      $hdrs = array_merge($hdrs, $m['headers']);
-      $send = $mail->send($mailto, $hdrs, $body);
-      if ($send !== TRUE && PEAR::isError($send)) {
-          airt_msg(_("Error sendmail mail!"). ' - '.$send->getMessage());
-          exit(reload());
-      }
+			$mail = Mail::factory('sendmail', $mail_params);
+			if ($mail !== TRUE && PEAR::isError($mail)) {
+				 airt_msg(_("Error creating mail relay!"). ' - '.$mail->getMessage());
+				 exit(reload());
+			}
+			$hdrs = array_merge($hdrs, $m['headers']);
+			$send = $mail->send($mailto, $hdrs, $body);
+			if ($send !== TRUE && PEAR::isError($send)) {
+				 airt_msg(_("Error sendmail mail!"). ' - '.$send->getMessage());
+				 exit(reload());
+			}
 
-      addIncidentComment(array(
-         'comment'=>sprintf(_("Email sent to %s: %s"),
-            $to, $subject),
-         'incidentid'=>$incidentid));
-      generateEvent('postsendmail', array(
-         'incidentid'=>$incidentid,
-         'sender'=>htmlentities($from),
-         'recipient'=>$to,
-         'subject'=>$subject));
+			addIncidentComment(array(
+				'comment'=>sprintf(_("Email sent to %s: %s"),
+					$to, $subject),
+				'incidentid'=>$incidentid));
+			generateEvent('postsendmail', array(
+				'incidentid'=>$incidentid,
+				'sender'=>htmlentities($from),
+				'recipient'=>$to,
+				'subject'=>$subject));
 
-      /* check for default actions on template */
-      $actions = array();
-      if ($template == _('Use preferred template')) {
-         $t = getPreferredMailTemplateName($incidentid);
-      } else {
-         $t = $template;
-      }
+			/* check for default actions on template */
+			$actions = array();
+			if ($template == _('Use preferred template')) {
+				$t = getPreferredMailTemplateName($incidentid);
+			} else {
+				$t = $template;
+			}
 
-      if (!empty($t) && get_template_actions($t, $actions)) {
-         if ($actions['type'] == -1) {
-            $actions['type'] = '';
-         } else {
-            addIncidentComment(array(
-               'comment'=>sprintf(_('Type updated to %s'),
-                  getIncidentTypeLabelByID($actions['type'])),
-               'incidentid'=>$incidentid));
-         }
-         if ($actions['status'] == -1) {
-            $actions['status'] = '';
-         } else {
-            addIncidentComment(array(
-               'comment'=>sprintf(_('Status updated to %s'),
-                  getIncidentStatusLabelByID($actions['status'])),
-               'incidentid'=>$incidentid));
-         }
-         if ($actions['state'] == -1) {
-            $actions['state'] = '';
-         } else {
-            addIncidentComment(array(
-               'comment'=>sprintf(_('State updated to %s'),
-                  getIncidentStateLabelByID($actions['state'])),
-               'incidentid'=>$incidentid));
-         }
-         $incident = getIncident($incidentid);
-         $actions['template'] = $incident['template'];
-         $actions['desc'] = $incident['desc'];
-         updateIncident($incidentid, $actions);
-      }
+			if (!empty($t) && get_template_actions($t, $actions)) {
+				if ($actions['type'] == -1) {
+					$actions['type'] = '';
+				} else {
+					addIncidentComment(array(
+						'comment'=>sprintf(_('Type updated to %s'),
+							getIncidentTypeLabelByID($actions['type'])),
+						'incidentid'=>$incidentid));
+				}
+				if ($actions['status'] == -1) {
+					$actions['status'] = '';
+				} else {
+					addIncidentComment(array(
+						'comment'=>sprintf(_('Status updated to %s'),
+							getIncidentStatusLabelByID($actions['status'])),
+						'incidentid'=>$incidentid));
+				}
+				if ($actions['state'] == -1) {
+					$actions['state'] = '';
+				} else {
+					addIncidentComment(array(
+						'comment'=>sprintf(_('State updated to %s'),
+							getIncidentStateLabelByID($actions['state'])),
+						'incidentid'=>$incidentid));
+				}
+				$incident = getIncident($incidentid);
+				$actions['template'] = $incident['template'];
+				$actions['desc'] = $incident['desc'];
+				updateIncident($incidentid, $actions);
+			}
+		} // if $skipped
       if ($action == _('Send and prepare next') && isset($incidentids) &&
          isset($template)) {
-         $override = fetchFrom('REQUEST', 'override', '%d');
-         defaultTo($override, 0);
          reload(BASEURL.'/mailtemplates.php?action=prepare'.
-            '&template='.urlencode($template).
-            '&override='.urlencode($override). 
-            '&autosend='.$autosend.
-            '&incidentids='.urlencode($incidentids));
+            '&override='.urlencode($override));
       } else {
-         reload('incident.php');
+			$n = sizeof(array_keys($_SESSION['skipped']));
+			if ($n > 0) {
+				pageHeader(_('Error sending email'), array(
+					'menu'=>'mail',
+					'submenu'=>'templates'
+				));
+				echo '<div class="block">'.LF;
+				echo '<h3>'._('Unsent email').'</h3>'.LF;
+				echo '<p/>'.LF;
+				echo _('Email for the following incidents failed to send due to errors.').LF;
+				echo '<p/>'.LF;
+				echo '<ul>'.LF;
+				foreach (array_keys($_SESSION['skipped']) as $key) {
+					printf(t('<li><a href="%u/incident.php?action=details&incidentid=%d">%l</a></li>'.LF, array(
+						'%u'=>BASEURL,
+						'%d'=>$key,
+						'%l'=>normalize_incidentid($key))));
+				}
+				echo '</ul>'.LF;
+				echo '</div>'.LF;
+				printf("<a href=\"incident.php\">"._('Continue').LF);
+				pageFooter();
+				unset($_SESSION['skipped']);
+			} else {			
+         	reload('incident.php');
+			}
       }
 
       break;
