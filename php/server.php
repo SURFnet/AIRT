65b52dd908873d59faf6eb062eede5b6f1e6580d
Kees Leune <kees@leune.com>
2013-07-20 03:45:20 -0400

    - Import queue now only accepts jobs from localhost. All external jobs will be rejects - Import queue no longer uses authentication tickets. The table will be removed from the database in a later commit - One new options in the configuration file importuser Represents the AIRT userid of the user used to process automatic imports - AIRT will now process import queue elements as follows: if the constituency is not default, AND there is a know preferred mail template, AND the filter is listed, THEN automatically import the queue element and create a new incident.

--- php/server.php
+++ php/server.php
@@ -31,6 +31,12 @@ require_once LIBDIR.'/search.plib';
 require_once LIBDIR.'/profiler.plib';
 require_once LIBDIR.'/network.plib';
 
+if ($_SERVER['REMOTE_ADDR'] != '127.0.0.1') {
+   airt_profile('Refusing client from '.$_SERVER['REMOTE_ADDR']);
+	die('Access denied.');
+}
+airt_profile('SOAP: Accepted incoming connection');
+
 $server       = new SOAP_Server();
 $webservice   = new IncidentHandling();
 $server->addObjectMap($webservice,'http://schemas.xmlsoap.org/soap/envelope/');
@@ -58,10 +64,7 @@ class IncidentHandling {
    function IncidentHandling () {
       // Define the signature of the dispatch map on the Web services method
       // Necessary for WSDL creation
-      $this->__dispatch_map['RequestAuthentication'] = array(
-         'in'  => array('auth_request' => 'string'),
-         'out' => array('AuthenticationTicket' => 'string')
-      );
+	   airt_profile('Figuring out where to send incoming rquest');
 
       $this->__dispatch_map['getXMLIncidentData'] = array(
          'in'  => array('action' => 'string'),
@@ -81,78 +84,11 @@ class IncidentHandling {
       $this->__dispatch_map['addLogging'] = array(
          'in'  => array('incidentid' => 'integer',
                         'logging'=>'string',
-                        'template'=>'string',
-                        'AuthenticationTicket'=>'string'),
+                        'template'=>'string'),
          'out' => array('confirmation' => 'string')
       );
    }
 
-
-   function RequestAuthentication($auth_request) {
-      $dom = '';
-      $doc = new DOMDocument();
-      airt_profile('RequestAuthentication: parsing request');
-      $dom = $doc->loadXML($auth_request);
-      if (!$dom) {
-         $error = 'Could not parse XML document';
-         return $error;
-         exit;
-      }
-
-      if ($doc->hasChildNodes() == false) {
-         $error = 'XML does not contain any elements';
-         return $error;
-         exit;
-      }
-
-      $username_element = $doc->getElementsByTagname('username');
-      $password_element = $doc->getElementsByTagname('password');
-      if ($username_element->length == 0 || $password_element->length == 0) {
-         $error = 'Username element or password element is empty';
-         return $error;
-         exit;
-      }
-
-      $username = $username_element->item(0)->textContent;
-      $password = $password_element->item(0)->textContent;
-
-      airt_profile('RequestAuthentication: authenticating user');
-      $userid = airt_authenticate($username,$password);
-      if ($userid == -1) {
-         airt_profile('RequestAuthentication: credentials refused');
-         $error = 'Permission denied';
-         return $error;
-         exit;
-      }
-      airt_profile('RequestAuthentication: credentials accepted');
-
-      // generate a new random id
-      $ticketid = genRandom();
-
-      // put it in the db
-      $creationid = CreateTicket($userid,$ticketid);
-      $issuetime = getIssueTime($creationid);
-
-      // format it to XML-standards
-      $issuetime = substr($issuetime,0,10)."T".substr($issuetime,11,8);
-      $exptime = date('Y-m-d\TH:i:s',mktime(substr($issuetime,11,2),
-         substr($issuetime,14,2), substr($issuetime,17,2),
-         substr($issuetime,5,2), substr($issuetime,8,2),
-         substr($issuetime,0,4)) + TICKET_EXP);
-
-      $ticket_details = array();
-      $ticket_details['creationid'] = $creationid;
-      $ticket_details['assertionid'] = WS_ENDPOINT.'?'.$creationid;
-      $ticket_details['ticketid'] = $ticketid;
-      $ticket_details['issuetime'] = $issuetime;
-      $ticket_details['exptime'] = $exptime;
-
-      // generate a SAML-ticket
-      $saml_ticket = generateSAMLTicket($ticket_details);
-
-      // send it back to the user
-      return $saml_ticket;
-   }
 
    function getXMLIncidentData($action)  {
       if ($action == 'getAll') {
@@ -161,221 +97,16 @@ class IncidentHandling {
       }
    }
 
-   function importIncidentData($importXML) {
-      $dom = $mailtemplate = $state = $status = $type = $incidentid = $address = $ip = $addressrole ='';
-
-      // first check the validity of the XML
-      $doc = new DOMDocument();
-      $dom = $doc->loadXML($importXML);
-      if (!$dom) {
-         $error = 'Could not parse XML document';
-         return $error;
-         exit;
-      }
-airt_profile('parsed');
-
-      if ($doc->hasChildNodes() == false) {
-         $error = 'XML does not contain any elements';
-         return $error;
-         exit;
-      }
-
-airt_profile('xml ok');
-
-      // check the credentials
-      $message_ident_el = $doc->getElementsByTagname('messageIdentification');
-      if ($message_ident_el->length == 0) {
-         $error = 'Empty messageIdentification element';
-         return $error;
-         exit;
-      }
-
-airt_profile('identification ok');
-
-      if ($message_ident_el->length > 0) {
-         $ticket_el = $message_ident_el->item(0)->getElementsByTagname('TicketID');
-         if ($ticket_el->length == 0) {
-            $error = 'Key element is empty';
-            return $error;
-            exit;
-         } else {
-            $ticketid = $ticket_el->item(0)->textContent;
-            $userid = CheckCredentials($ticketid);
-            if ($userid == -1) {
-               return 'Not authorized';
-            }
-            $_SESSION['userid'] = $userid;
-         }
-      }
-
-airt_profile('Credentials ok');
-
-      // get defaults
-      if (!defined('WS_IMPORT_DEFAULTSTATE')) {
-         $state = getIncidentStateDefault();
-         if($state == null) {
-            setIncidentStateDefault();
-            $state = getIncidentStateDefault();
-         }
-      } else {
-         $state = array_search(WS_IMPORT_DEFAULTSTATE, getIncidentStates());
-         if ($state == false) {
-            $state = getIncidentStateDefault();
-         }
-      }
-
-airt_profile('State: '.$state);
-
-      $status = getIncidentStatusDefault();
-      if($status == null) {
-         setIncidentStatusDefault();
-         $status = getIncidentStatusDefault();
-      }
-
-airt_profile('Status: '.$status);
-
-      $type = getIncidentTypeDefault();
-      if($type == null) {
-         setIncidentTypeDefault();
-         $type = getIncidentTypeDefault();
-      }
-
-airt_profile('Type: '.$type);
-
-      // parse incident data
-      $incidents = $doc->getElementsByTagname('incident');
-      for ($count=0;$count<$incidents->length;$count++) {
-         // begin ticketInformation
-         $ti = $incidents->item($count)->getElementsByTagName('ticketInformation');
-         if ($ti->length > 0) {
-            $prefixel = $ti->item(0)->getElementsByTagname('prefix');
-            if ($prefixel->length > 0) {
-               $prefix = $prefixel->item(0)->textContent;
-               if (empty($prefix)) {
-                  $prefix = '#UNKNOWN';
-               }
-            }
-            $refel = $ti->item(0)->getElementsByTagname('reference');
-            if ($refel->length > 0) {
-               $reference = $refel->item(0)->textContent;
-               if (empty($reference)) {
-                  $reference = '0';
-               }
-            }
-         } // end TicketInformation
-
-         $techinfo = $incidents->item($count)->getElementsByTagname('technicalInformation');
-         for ($count2=0; $count2<$techinfo->length; $count2++) {
-            $ip_element = $techinfo->item($count2)->getElementsByTagname('ip');
-            if ($ip_element->length > 0) {
-               $ip = $ip_element->item(0)->textContent;
-               // default ip
-               if ($ip == null) {
-                  $ip = '127.0.0.1';
-               }
-            }
-            $hostname_element = $techinfo->item($count2)->getElementsByTagname('hostname');
-            if ($hostname_element->length > 0) {
-               $hostname = $hostname_element->item(0)->textContent;
-               // default hostname
-               if ($hostname == null) {
-                  $hostname = 'localhost';
-               }
-            }
-
-            $time_dns_resolving_element = $techinfo->item($count2)->getElementsByTagname('time_dns_resolving');
-            if ($time_dns_resolving_element->length > 0) {
-               $time_dns_resolving = $time_dns_resolving_element->item(0)->textContent;
-               // default time_dns_resolving
-               if ($time_dns_resolving == null) {
-                  $time_dns_resolving = time();
-               }
-            }
-
-            $incident_time_element = $techinfo->item($count2)->getElementsByTagname('incident_time');
-            if ($incident_time_element->length > 0) {
-               $incident_time = $incident_time_element->item(0)->textContent;
-               // default incident_time
-               if ($incident_time == null) {
-                  $incident_time = time();
-               }
-            }
-
-            $logging_element = $techinfo->item($count2)->getElementsByTagname('logging');
-            if ($logging_element->length > 0) {
-               $logging = $logging_element->item(0)->textContent;
-            }
-airt_profile('Logging: ok');
-
-            $mailtemplate_element = $techinfo->item($count2)->getElementsByTagname('mailtemplate');
-            if ($mailtemplate_element->length > 0) {
-               $mailtemplate = $mailtemplate_element->item(0)->textContent;
-            }
-airt_profile('Mailtemplate: ok');
-
-            $address = $ip;
-            $addressrole = '0';
-         } // end technicalInformation
-      } // end incident
-
-airt_profile('Incident data parsed');
-
-      // generate an incident id
-      $i = 0 ; // Do we need this index/key ?
-      $incidentid[$i] = createIncident(array(
-         'state'=>$state,
-         'status'=>$status,
-         'type'=>$type,
-         'logging'=>$logging));
-airt_profile('Incident '.$incidentid[$i].' created');
-
-      addIPtoIncident(array(
-			'ip'=>$address,
-		   'hostname'=>$hostname,
-			'incidentid'=>$incidentid[$i],
-			'addressrole'=>$addressrole));
-airt_profile('IP addresses added');
-
-      $networkid = categorize($address);
-      $constituencyID = getConstituencyIDbyNetworkID($networkid);
-      $contacts = getConstituencyContacts($constituencyID);
-      foreach ($contacts as $id=>$data) {
-         addUserToIncident($data['userid'], $incidentid[$i]);
-      }
-airt_profile('Users added');
-
-      if ($mailtemplate != '' && $mailtemplate != _('No preferred template')) {
-         setPreferredMailTemplateName($incidentid[$i], $mailtemplate);
-         addIncidentComment(array(
-            'comment'=>'Import queue set preferred template to: '.$mailtemplate,
-            'incidentid'=>$incidentid[$i]));
-      }
-
-airt_profile('Template added');
-
-      if (!isset($error)) {
-         airt_profile('Success');
-         $error = 'Import successful. Imported incident with id ';
-         $id_list = '';
-         foreach ($incidentid as $i => $id)
-            $id_list .= "$id, ";
-         $id_list = rtrim($id_list,', ');
-         $error .= $id_list.'.';
-         return $error;
-      }
-   }
-
    /* Add log information to existing incident
     *
     * $incidentid (numeric) = Incident to add to
     * $logging (string) = Logging to be added
-    * $authticket (string) = SAML Authentication ticket for the request
     *
     * Returns a string containing a descriptive error message
     * in case of failure, or the empty string in case of success.
     */
-   function addLogging($incidentid, $logging, $template, $authticket) {
-      $userid = CheckCredentials($authticket);
+   function addLogging($incidentid, $logging, $template) {
+      $userid = IMPORTUSER;
 
       if (!is_string($logging)) {
          return 'Invalid data type (logging)';
@@ -383,15 +114,8 @@ airt_profile('Template added');
       if (!is_numeric($incidentid)) {
          return 'Invalid data type (incidentid)';
       }
-      if (!is_string($authticket)) {
-         return 'Invalid data type ($authticket)';
-      }
       if (!is_string($template)) {
          return 'Invalid data type ($template)';
-      }
-      $userid = CheckCredentials($authticket);
-      if ($userid == -1) {
-         return 'Not authorized.';
       }
       if (($incident = getIncident($incidentid)) == false) {
          return 'No such incident ($incidentid)';
@@ -594,6 +318,11 @@ airt_profile('Template added');
       }
       return 'SUCCESS';
    }
+
+	function importIncidentData($xml) {
+		airt_profile('Web service method importIncidentData');
+		return AIRT_importIncidentData($xml);
+	}
 } // end of class IncidentHandling
 
 
