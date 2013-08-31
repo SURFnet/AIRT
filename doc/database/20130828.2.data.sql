9286780ea3fef506cc7b9c4e3943e776d87fbd0c
Xander Jansen <Xander.Jansen@surfnet.nl>
2013-08-31 16:37:42 +0200

    Add doc/database/20130828.2.data.sql

---
+++ doc/database/20130828.2.data.sql
@@ -0,0 +1,6 @@
+UPDATE versions SET value='20130828.2' WHERE key='airtversion';
+
+INSERT INTO settings (key, value) VALUES ('inqueuemerge', 1);
+
+INSERT INTO settings (key, value) VALUES ('inqueuesep', null);
+
