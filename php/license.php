f08bd99fd400549a428f761b85307ce6d4403ce4
Thijs Kinkhorst <thijs@kinkhorst.com>
2025-12-25 16:56:27 +0100

    Cleanup unused code

--- php/license.php
+++
@@ -1,9 +0,0 @@
-<?php
-   $public = 1;
-	 require_once 'config.plib';
-   require_once LIBDIR.'/airt.plib';
-   pageHeader("GNU General Public License");
-   echo "<pre>";
-   include "/usr/share/common-licenses/GPL";
-   echo "</pre>";
-   pageFooter();
