779df96b0ac798a65ba135c76c2eb04fe1381375
Thijs Kinkhorst <thijs@kinkhorst.com>
2026-01-13 15:12:04 +0100

    Import queue: increase size of some fields to allow shadowserver import with lots of subtags

---
+++ doc/database/20260113.1.schema.sql
@@ -0,0 +1,3 @@
+alter table import_queue alter column type type varchar(255);
+alter table import_queue alter column summary type varchar(255);
+alter table import_queue alter column version type varchar(255);
